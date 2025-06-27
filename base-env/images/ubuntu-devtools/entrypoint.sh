84c0246fe30ace62d167aa76620e2b557b7d97b8
kurt <mrkurt@gmail.com>
2025-06-27 09:09:54 -0500

    try inner container as non root again

--- base-env/images/ubuntu-devtools/entrypoint.sh
+++ base-env/images/ubuntu-devtools/entrypoint.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 set -e
 
-export PATH="/usr/local/bin:\
-/usr/bin:\
-/bin:\
-/usr/local/sbin:\
-/usr/sbin:\
-/sbin:\
-/home/sprite/.local/bin:\
-$PATH"
-
-
-echo "127.0.0.1	sprite" >> /etc/hosts
 # Set up mounts using sudo (privileged operations)
-/.sprite/setup-mounts.sh
+sudo /.sprite/setup-mounts.sh
 # Not sprite — capture args in env and re-exec as sprite
-exec gosu "$TARGET_USER" "$@"
+exec "$@"
