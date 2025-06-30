e63a2a5acb6872d52addb52f8748810d293630e3
kurt <mrkurt@gmail.com>
2025-06-30 09:49:00 -0500

    use tini from entrypoint

--- base-env/images/ubuntu-devtools/root/.sprite/entrypoint.sh
+++ base-env/images/ubuntu-devtools/root/.sprite/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 # Set up mounts using sudo (privileged operations)
 sudo /.sprite/setup-mounts.sh
 # Not sprite — capture args in env and re-exec as sprite
-exec "$@"
+exec /.pilot/tini -- "$@"
